b19bb8221bd95683d8efa922db921d86a5c46db6
corybrodsky-svg <cory.brodsky@gmail.com>
2026-03-24 12:47:20 -0400

    Fix home dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,3 @@
-"use client";
-
 export default function LoginPage() {
-  return (
-    <div style={{ color: "white", padding: "40px", background: "black", minHeight: "100vh" }}>
-      NEW LOGIN PAGE TEST
-    </div>
-  );
+  return <div style={{ color: "white", padding: 40, background: "black", minHeight: "100vh" }}>LOGIN WORKS NOW</div>;
 }
