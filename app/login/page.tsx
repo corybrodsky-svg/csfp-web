a2bc25423ee8afced7037f36e414d8fb4254e54f
corybrodsky-svg <cory.brodsky@gmail.com>
2026-03-26 11:22:14 -0400

    fix login import path

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,7 +3,7 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { supabase } from "../../lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 
 const pageStyle: React.CSSProperties = {
   minHeight: "100vh",
